cb4cd8daf4f2d5164a382b17090854ad7e27aa55
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-01-05 23:05:39 -0300

    Primeiro endpoint - client

--- backend/src/main/java/com/projetojefferson/dsclient/entities/Client.java
+++ backend/src/main/java/com/projetojefferson/dsclient/entities/Client.java
@@ -4,10 +4,10 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+
 public class Client implements Serializable {
-
-
-	private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name, cpf;
 	private Double income;
@@ -31,6 +31,55 @@ public class Client implements Serializable {
 	}
 
 	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
--- backend/src/main/java/com/projetojefferson/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/projetojefferson/dsclient/resources/ClientResource.java
@@ -1,32 +1,39 @@
 package com.projetojefferson.dsclient.resources;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.projetojefferson.dsclient.entities.Client;
-import com.projetojefferson.dsclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
+
 public class ClientResource {
 
-	@Autowired
-	private ClientService service;
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findall(){
+	public ResponseEntity<List<Client>> findall() throws ParseException{
 
-		Date data = new Date();
 		List<Client> list = new ArrayList<>();
 		
-		list.add(new Client("jefferson"));
+		String name = "Jefferon";
+		String cpf = "42988244898";
+		Double income = 22.5;
+		Date birthDate = sdf.parse("02/01/1995");
+		Integer children = 10;
+		
+		list.add(new Client(1L, name, cpf, income, birthDate, children));
+		
 		
 		return ResponseEntity.ok().body(list);
 	}
