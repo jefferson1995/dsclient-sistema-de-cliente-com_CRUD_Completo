81252737b078015254410f9a25fe859a257cf833
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-01-07 21:57:39 -0300

    Classe DTO

--- backend/src/main/java/com/projetojefferson/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/projetojefferson/dsclient/resources/ClientResource.java
@@ -1,7 +1,6 @@
 package com.projetojefferson.dsclient.resources;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projetojefferson.dsclient.entities.Client;
-import com.projetojefferson.dsclient.resources.services.ClientService;
+import com.projetojefferson.dsclient.dto.ClientDTO;
+import com.projetojefferson.dsclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -23,13 +22,8 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findall() throws ParseException{
-
-		List<Client> list = service.findAll();
-		
-	
-		
-		
+	public ResponseEntity<List<ClientDTO>> findall() throws ParseException{
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
--- backend/src/main/java/com/projetojefferson/dsclient/services/ClientService.java
+++ backend/src/main/java/com/projetojefferson/dsclient/services/ClientService.java
@@ -1,11 +1,13 @@
-package com.projetojefferson.dsclient.resources.services;
+package com.projetojefferson.dsclient.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.projetojefferson.dsclient.dto.ClientDTO;
 import com.projetojefferson.dsclient.entities.Client;
 import com.projetojefferson.dsclient.repositories.ClientRepository;
 
@@ -16,9 +18,9 @@ public class ClientService{
 	private ClientRepository repository;
 	
 	@Transactional(readOnly=true)
-	public List<Client> findAll(){
-		
-		return repository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 	
 }
