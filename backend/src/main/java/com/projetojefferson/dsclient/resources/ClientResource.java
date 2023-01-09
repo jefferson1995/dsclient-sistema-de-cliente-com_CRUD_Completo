85d406e59e399c07f134d4c10621452a19d7ed28
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-01-09 20:35:23 -0300

    endpoint - inserir novo cliente

--- backend/src/main/java/com/projetojefferson/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/projetojefferson/dsclient/resources/ClientResource.java
@@ -1,13 +1,18 @@
 package com.projetojefferson.dsclient.resources;
 
+import java.net.URI;
 import java.text.ParseException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetojefferson.dsclient.dto.ClientDTO;
 import com.projetojefferson.dsclient.services.ClientService;
@@ -16,15 +21,37 @@ import com.projetojefferson.dsclient.services.ClientService;
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
-	
-	
 	@Autowired
 	private ClientService service;
-	
+
+	// Método para buscar todos clientes
 	@GetMapping
-	public ResponseEntity<List<ClientDTO>> findall() throws ParseException{
+	public ResponseEntity<List<ClientDTO>> findall() throws ParseException {
 		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+
+	// Método para buscar por id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+
+		ClientDTO dto = service.findByID(id);
+
+		return ResponseEntity.ok().body(dto);
+
+	}
+
+	// Método para inserir novo cliente
 	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+
+		dto = service.insert(dto);
+
+		// Código com o endereço dos dados que estão sendo adicionado, nesse caso o id
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(dto);
+	}
+
 }
--- backend/src/main/java/com/projetojefferson/dsclient/services/ClientService.java
+++ backend/src/main/java/com/projetojefferson/dsclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.projetojefferson.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +13,44 @@ import com.projetojefferson.dsclient.entities.Client;
 import com.projetojefferson.dsclient.repositories.ClientRepository;
 
 @Service
-public class ClientService{
-	
+public class ClientService {
+
 	@Autowired
 	private ClientRepository repository;
-	
-	@Transactional(readOnly=true)
-	public List<ClientDTO> findAll(){
+
+	// Método para buscar dados paginados
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
-	
+
+	// Método para buscar dados por ID
+	@Transactional(readOnly = true)
+	public ClientDTO findByID(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow();
+		return new ClientDTO(entity);
+	}
+
+	// Método para salvar um novo cliente.
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity); // método auxiliar
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client entity) { // Método auxiliar para adicionar um novo client ou
+																	// fazer
+																	// update
+		// Método irá pegar todos os dados do DTO e inserir na entidade Product
+
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setIncome(dto.getIncome());
+		entity.setChildren(dto.getChildren());
+
+	}
 }
